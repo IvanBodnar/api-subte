fdf1c965ddc25458cb5d0547b1d70d00de7cd3c8
ivan <ibodnar@outlook.com>
2018-12-06 16:55:26 -0300

    Agregado endpoint de estaciones en routes

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const lineaControllers = require('../controllers/linea.controller');
+const estacionControllers = require('../controllers/estacion.controller');
 
 const router = express.Router();
 
 router.get('/lineas', lineaControllers.getLineas);
+
+router.get('/estaciones', estacionControllers.getEstaciones);
 
 module.exports = router;
