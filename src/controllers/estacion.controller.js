680180c2ceb7a27aff683c1de791e9dbb7b79619
ivan <ibodnar@outlook.com>
2018-12-06 16:51:07 -0300

    Agregado controller getEstaciones

--- src/controllers/estacion.controller.js
+++ src/controllers/estacion.controller.js
@@ -1,1 +1,16 @@
 const Estacion = require('../db/models').Estacion;
+
+
+const getEstaciones = async (req, res, next) => {
+    try {
+        const estaciones = await Estacion.find({});
+        res.send(estaciones);
+    }
+    catch(e) {
+        res.send(e)
+    }
+};
+
+module.exports = {
+    getEstaciones: getEstaciones
+};
