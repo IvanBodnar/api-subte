e64e871d669d062c62e035efcfa61bbfa6cffd6f
ivan <ibodnar@outlook.com>
2018-12-07 09:20:23 -0300

    Agregado cross origin en main.js

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ const app = express();
 
 app.use( express.json() );
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization');
+    next();
+});
+
 app.use('/', routes);
 connection();
 
